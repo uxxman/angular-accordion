8f7d9ed32593ea5ae1d19f4080f8a83e2b1b9646
sherwaniusman <uxman.sherwani@gmail.com>
2014-09-25 17:39:01 +0500

    Added feature for using css font icons

--- js/ang-accordion.js
+++ js/ang-accordion.js
@@ -13,18 +13,20 @@ angular.module('angAccordion', ['collapsibleItem']).directive('angAccordion', fu
     replace: true,
     scope: {
       oneAtATime: '@',
-      rightIconUrl: '@',
-      downIconUrl: '@', 
+      closeIconUrl: '@',
+      openIconUrl: '@',
+      closeIconClass: '@',
+      openIconClass: '@',
       iconPosition: '@' 
     },
     controller: function($scope) {
       var collapsibleItems = [];
-
+      
       this.openCollapsibleItem = function(collapsibleItemToOpen) {
-        if($scope.oneAtATime) {
+        if( $scope.oneAtATime ) {
           angular.forEach(collapsibleItems, function(collapsibleItem) {
             collapsibleItem.isOpenned = false;
-            collapsibleItem.icon = $scope.rightIconUrl;
+            collapsibleItem.icon = collapsibleItem.closeIcon;
           });
         }
         collapsibleItemToOpen.isOpenned = true;
@@ -32,8 +34,18 @@ angular.module('angAccordion', ['collapsibleItem']).directive('angAccordion', fu
 
       this.addCollapsibleItem = function(collapsibleItem) {
         collapsibleItems.push(collapsibleItem);
-        collapsibleItem.rightIconUrl = $scope.rightIconUrl;
-        collapsibleItem.downIconUrl = $scope.downIconUrl;
+        
+        if ( $scope.closeIconClass != undefined || $scope.openIconClass != undefined ) {
+          collapsibleItem.iconsType = 'class';
+          collapsibleItem.closeIcon = $scope.closeIconClass;
+          collapsibleItem.openIcon = $scope.openIconClass;
+        }
+        else if ( $scope.closeIconUrl != undefined || $scope.openIconUrl != undefined ) {
+          collapsibleItem.iconsType = 'url';
+          collapsibleItem.closeIcon = $scope.closeIconUrl;
+          collapsibleItem.openIcon = $scope.openIconUrl;
+        }
+
         collapsibleItem.iconIsOnLeft = $scope.iconPosition == 'left' ? true: false;
       };
     },
@@ -54,23 +66,27 @@ angular.module('collapsibleItem', []).directive('collapsibleItem', function() {
     link: function(scope, element, attrs, accordionController) {
       scope.isOpenned = (scope.initiallyOpen) ? true : false;
       accordionController.addCollapsibleItem(scope);
-
+      
       if(scope.isOpenned)
-        scope.icon = scope.downIconUrl;
+        scope.icon = scope.openIcon;
       else
-        scope.icon = scope.rightIconUrl;
+        scope.icon = scope.closeIcon;
 
       scope.toggleCollapsibleItem = function () {
         if(!scope.isOpenned) {
           accordionController.openCollapsibleItem(this);
-          scope.icon = scope.downIconUrl;
+          scope.icon = scope.openIcon;
         }
         else {
           scope.isOpenned = false;
-          scope.icon = scope.rightIconUrl;
+          scope.icon = scope.closeIcon;
         }
+      };
+
+      scope.getIconUrl = function ( type ) {
+        return type == 'url' ? scope.icon : null;
       }
     },
-    template: '<div class="collapsible-item" ng-class="{open: isOpenned}"><div class="title" ng-click="toggleCollapsibleItem()">{{title}}<img class="icon" ng-class="{iconleft: iconIsOnLeft}" ng-src="{{icon}}" /></div><div class="body"><div class="content" ng-transclude></div></div></div>'
+    template: '<div class="collapsible-item" ng-class="{open: isOpenned}"><div class="title" ng-click="toggleCollapsibleItem()">{{title}}<i ng-show="iconsType == \'class\'" class="{{icon}} icon" ng-class="{iconleft: iconIsOnLeft}"></i><img ng-show="iconsType == \'url\'" class="icon" ng-class="{iconleft: iconIsOnLeft}" ng-src="{{getIconUrl(iconsType)}}" /></div><div class="body"><div class="content" ng-transclude></div></div></div>'
   };
 });
